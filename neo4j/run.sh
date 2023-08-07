e424ccc1228f7cef4eb5a41b26428373df27784b
Lhui010 <870033938@qq.com>
2023-08-07 15:51:35 +0800

    add: neo4j

---
+++ neo4j/run.sh
@@ -0,0 +1,11 @@
+sudo docker run \
+    --name neo4j \
+    -p33473:7473 -p33474:7474 -p33687:7687 \
+    -d \
+    -v $HOME/neo4j/data:/data \
+    -v $HOME/neo4j/logs:/logs \
+    -v $HOME/neo4j/import:/var/lib/neo4j/import \
+    -v $HOME/neo4j/plugins:/plugins \
+    --env NEO4J_AUTH=neo4j/password \
+    neo4j:latest
+
